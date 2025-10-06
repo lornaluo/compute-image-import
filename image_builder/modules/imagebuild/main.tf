24a408c75feefc865d6762c520c15e129e8d3ff6
shuoyiluo <shuoyiluo@google.com>
2025-10-06 14:59:39 -0700

    Add `update-user-metadata` step during new image creation

--- image_builder/modules/imagebuild/main.tf
+++ image_builder/modules/imagebuild/main.tf
@@ -92,6 +92,7 @@ resource "google_cloudbuild_trigger" "image_build_trigger" {
         var.customization_script_source == "" && var.testing_script_source == "" ? [] : [local.copy_packer_scripts_step],
         var.customization_script_source != "" ? [local.customization_init_step, local.customization_step] : [],
         local.add_label_step,
+        local.add_metadata_step,
         local.delete_temp_source_image_step,
         var.testing_script_source != "" ? [local.test_init_step, local.test_step] : [],
         var.target_image_family != "" ? [local.set_target_family_step] : []
@@ -263,6 +264,13 @@ locals {
     env  = []
     args = ["gcloud", "compute", "images", "add-labels", "$${_SUFFIXED_TARGET_IMAGE_NAME}", "--labels=pipeline=$${TRIGGER_NAME},build-id=$${BUILD_ID}"]
     id   = "add-pipeline-label"
+  }
+  add_metadata_step = {
+    name = "gcr.io/google.com/cloudsdktool/cloud-sdk"
+    env = []
+    args = ["gcloud", "compute", "images", "update", "$${_SUFFIXED_TARGET_IMAGE_NAME}", "--update-user-metadata=golden-image-source=projects/$${_SOURCE_IMAGE_OS}/global/images/$${_SOURCE_IMAGE}"
+ ]
+    id = "add-source-image-metadata"
   }
   delete_temp_source_image_step = {
     name = "gcr.io/google.com/cloudsdktool/cloud-sdk"
